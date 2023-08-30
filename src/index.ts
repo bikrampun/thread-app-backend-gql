5b756c3c3759f4f2f9d7b6bcc056e0d3750e234b
Bikram Pun <bikrampun47@gmail.com>
2023-08-30 22:28:35 +0545

    GraphQl Server Setup

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+
+async function startApolloServer() {
+  const app = express();
+  const port = Number(process.env.PORT) || 3000;
+
+  app.use(express.json());
+
+  // create GraphQL server
+  const gqlServer = new ApolloServer({
+    typeDefs: `
+        type Query {
+            hello: String
+        }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello world! This is GraphQL server",
+      },
+    },
+  });
+
+  await gqlServer.start();
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
+
+  app.use("/graphql", expressMiddleware(gqlServer));
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
+
+startApolloServer();
